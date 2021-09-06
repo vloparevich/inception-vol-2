55fe08e45b173e775b8be77d133da3a2b7e4f32c
vloparevich <vadim.loparevich@gmail.com>
2021-09-05 18:06:22 -0700

    Init the project

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("inception-vol-2 JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/inception-vol-2";
+
+module.exports = MONGO_URI;
