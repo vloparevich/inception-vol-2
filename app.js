07cf5f03723e041a8776b4ca0feeab6d02f4e91e
Fernando Salcedo <fsalcedo55@hotmail.com>
2021-09-08 06:41:10 -0400

    Fix path to auth routes on app.js

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 const index = require("./routes/index");
 app.use("/", index);
 
-const authRoutes = require("./routes/auth");
-app.use("/auth", authRoutes);
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth.routes", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
